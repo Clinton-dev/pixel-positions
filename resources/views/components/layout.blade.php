b3e6d0745ea9bf54f2ca74345ac2d52bf651d618
Clinton Dev <clintonwambugu@gmail.com>
2026-01-02 13:22:08 +0300

    Add tailwind & general layout file

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,33 @@
+@php use Illuminate\Support\Facades\Vite; @endphp
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+<div>
+    <nav>
+        <div>
+            <a href="">
+                <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="Pixel Position Logo">
+            </a>
+        </div>
+        <div>
+            <a href="">Jobs</a>
+            <a href="">Careers</a>
+            <a href="">Salaries</a>
+            <a href="">Companies</a>
+        </div>
+        <div>
+            <a href="">Post a Job</a>
+        </div>
+    </nav>
+    <main>
+        {{$slot}}
+    </main>
+</div>
+</body>
+</html>
